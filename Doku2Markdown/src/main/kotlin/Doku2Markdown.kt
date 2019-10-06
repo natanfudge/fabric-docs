7e4909497ec4af9f24a1ef0a79ce1296ae9aa6aa
natanfudge <natan.lifsiz@gmail.com>
2019-10-06 16:01:27 +0300

    add authors

--- Doku2Markdown/src/main/kotlin/Doku2Markdown.kt
+++ Doku2Markdown/src/main/kotlin/Doku2Markdown.kt
@@ -2,6 +2,7 @@ import it.skrape.extract
 import it.skrape.selects.element
 import it.skrape.selects.elements
 import it.skrape.skrape
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.internal.StringSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
@@ -10,11 +11,12 @@ import kotlinx.serialization.set
 import org.jsoup.Connection
 import org.jsoup.Jsoup
 import java.io.File
+import java.io.FileWriter
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
-import javax.imageio.ImageIO
+import java.util.*
 
 
 //pandoc --from dokuwiki --to gfm
@@ -25,18 +27,21 @@ const val LoginCookieValue = "DOKUWIKI_PASSWORD_HASH"
 
 const val ImageListPath = "$Resources/images.json"
 
+const val AuthorsPath = "$Resources/authors.json"
+
+val JsonConfig = Json(JsonConfiguration.Stable.copy(prettyPrint = true))
 
 
 fun scrapeAndWriteDokuWikiPages() {
     for (page in Json(JsonConfiguration.Stable).parse(
-        Page.serializer().list,
-        File(Pages).readText()
+            Page.serializer().list,
+            File(Pages).readText()
     )) {
         val url = page.editUrl
         val res = Jsoup.connect(url)
-            .data(LoginCookieKey, System.getenv(LoginCookieValue))
-            .method(Connection.Method.POST)
-            .execute()
+                .data(LoginCookieKey, System.getenv(LoginCookieValue))
+                .method(Connection.Method.POST)
+                .execute()
 
         val cookies = res.cookies()
         val doc = Jsoup.connect(url).cookies(cookies).get()
@@ -51,7 +56,7 @@ fun scrapeAndWriteDokuWikiPages() {
 
 fun scrapeAndWriteImageList() {
     val images = mutableSetOf<String>()
-    for (page in Json(JsonConfiguration.Stable).parse(Page.serializer().list, File(Pages).readText())) {
+    for (page in Page.getPages()) {
         val pageImages = skrape {
             url = page.url
 
@@ -63,19 +68,19 @@ fun scrapeAndWriteImageList() {
     }
 
     File(ImageListPath).writeText(
-        Json(JsonConfiguration.Stable.copy(prettyPrint = true)).stringify(
-            StringSerializer.set,
-            images
-        )
+            Json(JsonConfiguration.Stable.copy(prettyPrint = true)).stringify(
+                    StringSerializer.set,
+                    images
+            )
     )
 }
 
-fun downloadAndWriteImages(){
-    val images = Json(JsonConfiguration.Stable).parse(StringSerializer.list,File(ImageListPath).readText())
-        .filter { it.startsWith("/wiki/_media") }
+fun downloadAndWriteImages() {
+    val images = Json(JsonConfiguration.Stable).parse(StringSerializer.list, File(ImageListPath).readText())
+            .filter { it.startsWith("/wiki/_media") }
 
-    for(image in images){
-        val relativePath = image.removePrefix("/wiki/_media").split("?")[0].replace(":","/")
+    for (image in images) {
+        val relativePath = image.removePrefix("/wiki/_media").split("?")[0].replace(":", "/")
         val localPath = "$Resources/pages_markdown/images/$relativePath"
         File(localPath).parentFile.mkdirs()
         val httpsPath = URL("https://fabricmc.net$image")
@@ -86,12 +91,55 @@ fun downloadAndWriteImages(){
 
 }
 
+fun scrapeAuthors(page: Page): Set<String> {
+    val authors = mutableSetOf<String>()
+    var first = 0
+    do {
+        var nextPageExists = false
+        val authorsColumn = skrape {
+            url = page.revisionsUrl(first)
+
+            extract {
+                val lessRecentButton = elements(".no button").filter { it.attr("title") == "less recent >> [N]" }
+                if (lessRecentButton.isNotEmpty()) nextPageExists = true
+                elements("#page__revisions .user").map { it.text() }.toSet() + authors
+            }
+        }
+
+        authors.addAll(authorsColumn)
+        first += 20
+    } while (nextPageExists)
+
+    return authors
+}
+@Serializable
+data class PageAuthors(val page : Page, val authors : Set<String>)
+
+fun scrapeAndWriteAuthors(){
+    val authors = Page.getPages().map { PageAuthors(it,scrapeAuthors(it)) }
+    File(AuthorsPath).writeText(JsonConfig.stringify(PageAuthors.serializer().list,authors))
+}
+
+fun writeAllAuthors(){
+    val authors = JsonConfig.parse(PageAuthors.serializer().list,File(AuthorsPath).readText())
+    val allAuthors = authors.flatMap { it.authors }.toSet()
+    val properties = Properties()
+    for(author in allAuthors){
+        properties[author] = ""
+    }
+
+    FileWriter("$Resources/allAuthors.properties").use {
+        properties.store(it,"allAuthors")
+    }
+
+
+
+}
+
+
+//TODO: credits
+
 fun main() {
-    convertToMarkdown()
-
-//    ConfigFactory.parseFile()
-
-//    Hocon
-
-
+    writeAllAuthors()
+//    scrapeAndWriteAuthors()
 }
--- Doku2Markdown/src/main/kotlin/Page.kt
+++ Doku2Markdown/src/main/kotlin/Page.kt
@@ -4,8 +4,6 @@ import it.skrape.selects.elements
 import it.skrape.skrape
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.list
 import java.io.File
 
@@ -15,21 +13,31 @@ private const val MarkdownDirectory = "docs/"
 
 @Serializable
 data class Page(val tag: String?, val name: String) {
-    @Transient val url = "https://fabricmc.net/wiki/${if (tag == null) "" else "$tag:"}$name"
+    @Transient
+    val url = "https://fabricmc.net/wiki/${if (tag == null) "" else "$tag:"}$name"
+    @Transient
+    val editUrl = "$url?do=edit"
 
-    @Transient val editUrl  = "$url?do=edit"
+    fun revisionsUrl(first: Int = 0) = "$url?do=revisions&first=$first"
 
-    @Transient private val relativeDirectoryPath  = tag?.split(":")?.joinToString("/")?.plus("/") ?: ""
-    @Transient val localDokuWikiDirectory = "$Resources/pages_dokuwiki/$relativeDirectoryPath"
-    @Transient val localDokuWikiPath  = "$localDokuWikiDirectory$name.txt"
-
-
-
-    @Transient private val relativePath get() = migratePath("$relativeDirectoryPath$name.md")
-
+    @Transient
+    private val relativeDirectoryPath = tag?.split(":")?.joinToString("/")?.plus("/") ?: ""
+    @Transient
+    val localDokuWikiDirectory = "$Resources/pages_dokuwiki/$relativeDirectoryPath"
+    @Transient
+    val localDokuWikiPath = "$localDokuWikiDirectory$name.txt"
 
 
-    @Transient val localMarkdownPath  get() = "$MarkdownDirectory$relativePath"
+    @Transient
+    private val relativePath = migratePath("$relativeDirectoryPath$name.md")
+
+
+    @Transient
+    val localMarkdownPath = "$MarkdownDirectory$relativePath"
+
+    companion object {
+        fun getPages() = JsonConfig.parse(serializer().list, File(Pages).readText())
+    }
 }
 
 
@@ -37,7 +45,7 @@ fun notNamespaced(name: String) = Page(null, name)
 fun frenchTutorial(name: String) = Page("fr:tutoriel", name)
 
 const val Pages = "$Resources/pages.json"
-val BannedPages = listOf("dokuwiki","syntax","welcome","agenda","wiki_meta")
+val BannedPages = listOf("dokuwiki", "syntax", "welcome", "agenda", "wiki_meta")
 
 fun writePageList() {
     val tags = skrape {
@@ -50,9 +58,9 @@ fun writePageList() {
 
     val notNamespaced = listOf("changelog", "install", "rules", "sidebar_do_edit", "start", "wiki_meta")
     val frenchTutorials = listOf(
-        "ajouter_mods", "appliquer_modifications", "blocs", "enchantements",
-        "groupes_objets", "infobulles", "installation_java", "mise_en_place", "modpacks_atlauncher", "modpacks_technic",
-        "objets", "recettes", "termes"
+            "ajouter_mods", "appliquer_modifications", "blocs", "enchantements",
+            "groupes_objets", "infobulles", "installation_java", "mise_en_place", "modpacks_atlauncher", "modpacks_technic",
+            "objets", "recettes", "termes"
     )
 
     val pages = tags.flatMap { tag ->
@@ -65,17 +73,14 @@ fun writePageList() {
         }
     }.filter {
         it != Page(
-            "fr",
-            "tutoriel"
+                "fr",
+                "tutoriel"
         )
     } + notNamespaced.map { notNamespaced(it) } + frenchTutorials.map { frenchTutorial(it) }
 
 
 
     File(Pages).writeText(
-        Json(JsonConfiguration.Stable.copy(prettyPrint = true)).stringify(
-            Page.serializer().list,
-            pages.filter { it.name !in BannedPages }
-        )
+            JsonConfig.stringify(Page.serializer().list, pages.filter { it.name !in BannedPages })
     )
 }
